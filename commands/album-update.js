3b490008b62a81fe105203be11b1818cd0e6acd1
BayuDC <dev.bayudc@gmail.com>
2022-06-06 20:36:06 +0700

    feat: allow multiple words album name

--- commands/album-update.js
+++ commands/album-update.js
@@ -3,18 +3,18 @@ const Album = require('../models/album');
 module.exports = {
     name: 'album-update',
     /** @param {import('discord.js').Message} message */
-    async execute(message, name, key, value) {
+    async execute(message, name, key, ...value) {
         if (!name) return await message.channel.send('Album name is required.');
         if (!key) return await message.channel.send('Album key is required.');
-        if (!value) return await message.channel.send('Album value is required.');
+        if (!value.length) return await message.channel.send('Album value is required.');
 
         const album = await Album.findOne({ slug: name });
         if (!album) return await message.channel.send(`Album **${name}** does not exist.`);
 
-        album[key] = value;
+        album[key] = value.join(' ');
         await album.save();
 
         await message.channel.send(`Album **${name}** successfully updated.`);
-        await message.channel.send(`The new value for **${key}** is **${value}**.`);
+        await message.channel.send(`The new value for **${key}** is **${album[key]}**.`);
     },
 };
