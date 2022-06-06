3b7f1cdb4788c757dfa10f0cca7b39ef1195b66e
BayuDC <dev.bayudc@gmail.com>
2022-06-06 20:23:13 +0700

    feat: add `album-delete` command

---
+++ commands/album-delete.js
@@ -0,0 +1,20 @@
+const Album = require('../models/album');
+
+module.exports = {
+    name: 'album-delete',
+    /** @param {import('discord.js').Message} message */
+    async execute(message, name) {
+        if (!name) return await message.channel.send('Album name is required.');
+
+        const album = await Album.findOne({ name });
+
+        if (!album) return await message.channel.send(`Album **${name}** does not exist.`);
+
+        const channel = await message.client.channels.fetch(album.channelId);
+        await channel.delete();
+
+        await Album.deleteOne(album);
+
+        await message.channel.send(`Album **${name}** successfully deleted.`);
+    },
+};
