04b69198bfa0042b6d5525346e0c43f6519f9f0a
BayuDC <dev.bayudc@gmail.com>
2022-06-09 08:17:39 +0700

    feat: `get /albums`

--- controllers/album.js
+++ controllers/album.js
@@ -1,7 +1,47 @@
+const createError = require('http-errors');
+const Album = require('../models/album');
+const Picture = require('../models/picture');
+
 module.exports = {
-    load(req, res, next, id) {},
-    index(req, res, next) {},
-    show(req, res, next) {},
+    /**
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     * @param {import('express').NextFunction} next
+     * @param {String} id
+     */
+    async load(req, res, next, id) {
+        try {
+            const album = await Album.findById(id);
+            if (!album) throw undefined;
+
+            req.album = album;
+            return next();
+        } catch (error) {
+            next(createError(404, 'Album not found'));
+        }
+    },
+    /**
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
+    async index(req, res) {
+        const albums = await Album.find();
+        res.json({ albums });
+    },
+    /**
+     * @param {import('express').Request} req
+     * @param {import('express').Response} res
+     */
+    async show(req, res) {
+        const album = req.album;
+
+        res.json({
+            album: {
+                ...album.toJSON(),
+                picturesCount: await album.picturesCount,
+            },
+        });
+    },
     store(req, res, next) {},
     update(req, res, next) {},
     destroy(req, res, next) {},
