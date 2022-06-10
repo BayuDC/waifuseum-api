cbd138e6928a4e20a7e3a77fa113e6d0f4487d54
BayuDC <dev.bayudc@gmail.com>
2022-06-10 12:44:37 +0700

    chore: add gates for authorization

--- routes/album.js
+++ routes/album.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const validate = require('../middlewares/validate');
-const { guard } = require('../middlewares/auth');
+const { guard, gate } = require('../middlewares/auth');
 
 const controller = require('../controllers/album');
 const validation = require('../validations/album');
@@ -8,8 +8,8 @@ const validation = require('../validations/album');
 router.param('id', controller.load);
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', guard(), validate(validation.store), controller.store);
-router.put('/:id', guard(), validate(validation.update), controller.update);
-router.delete('/:id', guard(), controller.destroy);
+router.post('/', guard(), gate('album-create'), validate(validation.store), controller.store);
+router.put('/:id', guard(), gate('album-update'), validate(validation.update), controller.update);
+router.delete('/:id', guard(), gate('album-delete'), controller.destroy);
 
 module.exports = router;
