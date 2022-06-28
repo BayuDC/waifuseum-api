e195856872e60cdda48b9c41c1c046358e59dd92
BayuDC <dev.bayudc@gmail.com>
2022-06-29 01:27:08 +0700

    feat: implement new gate system

--- routes/album.js
+++ routes/album.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { guard, gate, own, can } = require('../middlewares/auth');
+const { guard, gate } = require('../middlewares/auth');
 
 const controller = require('../controllers/album');
 const validation = require('../validations/album');
@@ -8,13 +8,13 @@ router.param('id', controller.load);
 
 router.get('/', validation.index, controller.index);
 router.get('/mine', guard(), validation.index, controller.indexMine);
-router.get('/all', guard(), can('manage-album'), gate(), controller.indexAll);
+router.get('/all', guard(), gate(can('manage-album')), controller.indexAll);
 router.get('/:id', controller.show);
 
 router.use(guard());
 router.post('/', validation.store, controller.store);
 
-router.use('/:id', own('album'), can('manage-album'), gate());
+router.use('/:id', gate(own('album'), can('manage-album')));
 router.put('/:id', validation.update, controller.update);
 router.delete('/:id', controller.destroy);
 
--- routes/picture.js
+++ routes/picture.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { guard, gate, can, own } = require('../middlewares/auth');
+const { guard, gate } = require('../middlewares/auth');
 
 const controller = require('../controllers/picture');
 const validation = require('../validations/picture');
@@ -14,7 +14,7 @@ router.get('/:id', controller.show);
 router.use(guard());
 router.post('/', upload, download, validation.store, controller.store);
 
-router.use('/:id', own('picture'), can('manage-picture'), gate());
+router.use('/:id', gate(own('picture'), can('manage-picture')));
 
 router.put('/:id', upload, download, validation.update, controller.update);
 router.delete('/:id', controller.destroy);
